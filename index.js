8e174d067b8185a37fa0fee25566f5fbdd66e6a3
John Lloyd Quizeo <johnkiezquizeo@gmail.com>
2025-02-20 00:09:46 +0800

    updated css

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.set('views', path.join(__dirname, 'Views'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('Public'));
 
 const db = new Pool({
     connectionString: process.env.DATABASE_URL,
